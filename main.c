f307e116b0d19ae3765fa1f568a7e83b0e848543
zakrent <szymon.zakrent@gmail.com>
2018-01-13 22:39:53 +0100

    Added response with file

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
+#include <dirent.h>
 
 typedef struct{
 	char htmlVersion[9];
@@ -19,6 +20,7 @@ typedef struct{
 typedef struct {
 	char htmlVersion[9];
 	char statusCode[48];
+	int bodyLength;
 	char* headers;
 	char* body;
 } Response;
@@ -37,10 +39,75 @@ int initializeServerSocket(char ip[]){
 	return newSocket;
 }
 
+int readFile(char** returnedFileBuffer, char fileName[]){
+	// Return value:
+	// -1  = file not found
+	// -2  = internal error
+	// > 0 = buffer size
+	int fileNameLength = strlen(fileName);
+	char* systemFileName;
+
+	//when filename is / open index.html
+	if(fileNameLength < 2){
+	        char* defaultFileName = "index.html";
+		systemFileName = defaultFileName;
+	}
+	else{
+		systemFileName = fileName+1;
+	}	
+
+	//check if file exists
+	char* fileDirectoryName = "./static";
+	DIR* fileDirectory = opendir(fileDirectoryName); 
+	if(fileDirectory == NULL){
+		perror("Failed to open file directory!");
+		return -2;
+	}
+
+	struct dirent *entry;
+	while((entry = readdir(fileDirectory)) != NULL){
+		if(entry->d_type == DT_REG && strcmp(entry->d_name, systemFileName) == 0){
+			//generate file path
+			char *filePath = alloca(strlen(fileDirectoryName)+strlen(entry->d_name)+2);
+			sprintf(filePath, "%s/%s", fileDirectoryName, entry->d_name);
+
+			//open file
+			FILE *file;
+			file = fopen(filePath, "r");
+			if(file == NULL){
+				perror("Failed to open file!");
+				return -2;
+			}
+
+			//get file size
+			if(fseek(file, 0, SEEK_END) < 0){
+				perror("Failed to seek!");
+				return -2;
+			}
+			long fileSize = ftell(file);
+			rewind(file);
+
+			//read file
+			*returnedFileBuffer = malloc(fileSize);
+			if(fread(*returnedFileBuffer, 1, fileSize, file) != fileSize){
+				perror("Failed to read file!");
+			}		
+			return fileSize;
+		}
+	}
+	return -1;
+}
+
 void getResponseCodeString(int code, char* string){
 	switch(code){
 		case 200:
 			strcpy(string, "200 OK");
+			break;
+		case 404:
+			strcpy(string, "404 Not Found");
+			break;
+		case 500:
+			strcpy(string, "500 Internal Server Error");
 			break;
 		case 501:
 			strcpy(string, "501 Not Implemented");
@@ -51,8 +118,17 @@ void getResponseCodeString(int code, char* string){
 	}
 }
 
-void generateResponseString(char* string, Response response){
-	sprintf(string, "%s %s\r\n%s\r\n\%s", response.htmlVersion, response.statusCode, response.headers, response.body);
+char* generateResponseHeader(Response response){
+	char nullString[] = "";
+	if(response.headers == NULL){
+		response.headers = nullString;	
+	}
+
+	int responseLength = strlen(response.htmlVersion)+1+strlen(response.statusCode)+2+strlen(response.headers)+2;
+	char* string = malloc(responseLength);
+	memset(string, 0, responseLength);
+	sprintf(string, "%s %s\r\n%s\r\n", response.htmlVersion, response.statusCode, response.headers);
+	return string;
 }
 
 
@@ -71,21 +147,16 @@ void handleConnection(int socket){
 	
 	//Get request info
 	Request request;
+	memset(&request, 0, sizeof(request));
 	strncpy(request.method, strtok(buffer, " "), 3);	
 	strncpy(request.filename, strtok(NULL, " "), 31);	
-	strncpy(request.htmlVersion, strtok(NULL, " "), 8);	
+	strncpy(request.htmlVersion, strtok(NULL, " \r\n"), 8);
 
 	//Set up response 
 	Response response;
-
-	char* body = malloc(sizeof(char)*200);
-	char* headers = malloc(sizeof(char)*60);
-	memset(body, 0, 200);
-	memset(headers, 0, 60);
-	response.headers = headers;
-	response.body = body;
-
+	memset(&response, 0, sizeof(response));
 	strncpy(response.htmlVersion, "HTTP/1.1", 8);
+	response.bodyLength = 0;
 	
 	//Check for request validity and parameters
 	if(strcmp(request.htmlVersion, "HTTP/1.1") != 0){
@@ -95,23 +166,36 @@ void handleConnection(int socket){
 		getResponseCodeString(501, response.statusCode);
 	}
 	else{
-		getResponseCodeString(200, response.statusCode);
-		//TODO: output specified file rather than hard coded html
-		strcpy(response.body, "<html><body><h1>Hello, World!</h1></body></html>");
+		//get requested file
+		char* fileBuffer;
+		int ret = readFile(&fileBuffer, request.filename);
+		if(ret == -1){
+			getResponseCodeString(404, response.statusCode);
+		}
+		else if(ret < 0){
+			getResponseCodeString(500, response.statusCode);
+		}
+		else{
+			getResponseCodeString(200, response.statusCode);
+			response.bodyLength = ret;
+			response.body = fileBuffer;
+		}
 	}
 
-	//Form a response
-	char responseString[2048];
-	memset(buffer, 0, 2048);
+	printf("%s\n", request.filename);
 
-	generateResponseString(responseString, response);
+	//Form a header
 
-	printf("%s", responseString);
+	char* headerString = generateResponseHeader(response);
 
 	//Send the response
-	if(send(socket, responseString, strlen(responseString), 0) < 0)
+	if(send(socket, headerString, strlen(headerString), 0) < 0)
 		perror("Failed to send response!");
+	if(response.bodyLength > 0)
+		if(send(socket, response.body, response.bodyLength, 0) < 0)
+			perror("Failed to send response!");
 
+	free(headerString);
 	free(response.body);
 	free(response.headers);
 }
